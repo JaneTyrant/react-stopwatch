0ee80beb9230817b3447e4f2981d828604ee3559
TyrantJane <nonstop00066@gmail.com>
2022-07-27 22:36:10 +0300

    Create StopWatch component

---
+++ src/components/StopWatch/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from './StopWatch.module.css';
+
+class StopWatch extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default StopWatch;
