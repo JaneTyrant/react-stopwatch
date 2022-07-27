8c46cca01b96e1aa78da07aa799a80e6562d4773
TyrantJane <nonstop00066@gmail.com>
2022-07-27 23:32:03 +0300

    Refactor StopWatch with setTimeout

--- src/components/StopWatch/index.js
+++ src/components/StopWatch/index.js
@@ -17,11 +17,12 @@ class StopWatch extends Component {
     });
   };
   start = () => {
-    if (this.intervalId === null) {
-      this.intervalId = setInterval(this.tick, 1000);
-    }
+    console.log(this.intervalId);
+    document.getElementById("start").setAttribute("disabled", "disabled");
+    this.intervalId = window.setTimeout(this.tick, 1000);
   };
   stop = () => {
+    document.getElementById("start").removeAttribute("disabled");
     clearInterval(this.intervalId);
     this.intervalId = null;
   };
@@ -29,19 +30,29 @@ class StopWatch extends Component {
     this.stop();
     this.setState({ time: new Date(0, 0, 0, 0, 0, 0) });
   };
+  componentDidUpdate() {
+    if (this.intervalId !== null) {
+      this.start();
+    }
+  }
   componentWillUnmount() {
     this.reset();
   }
   render() {
     const { time } = this.state;
-    console.log("render");
     return (
       <article className={styles.container}>
-        <h2>{time.toLocaleTimeString("en-GB")}</h2>
+        <h2 className={styles.time}>{time.toLocaleTimeString("en-GB")}</h2>
         <div>
-          <button onClick={this.start}>start</button>
-          <button onClick={this.stop}>stop</button>
-          <button onClick={this.reset}>reset</button>
+          <button onClick={this.start} className={styles.button} id="start">
+            start
+          </button>
+          <button onClick={this.stop} className={styles.button}>
+            stop
+          </button>
+          <button onClick={this.reset} className={styles.button}>
+            reset
+          </button>
         </div>
       </article>
     );
